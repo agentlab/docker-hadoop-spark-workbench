29b07243c129fe30b637d12ad2b5e3755f647342
Alex Ivanov <amivanoff@gmail.com>
2017-08-02 20:19:59 +0300

    Version update

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 cd ../docker-spark/base
-docker build -t agentlab/spark-base:2.2.0-hadoop2.7 .
+docker build -t agentlab/spark-base:2.2.0-hadoop2.8.1 .
 cd ../../docker-hadoop-spark-workbench
+
+cd ../docker-hadoop/base
+docker build -t agentlab/hadoop-base:2.8.1 .
+cd ..
